1ba442c7b797427caf4b0b387007fc6a1b974369
James Weber <weber@flagship.cc>
2018-08-26 22:13:07 +0900

    basic admin and page functionality

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
+    @articles = Article.all
+  end
+  def show
+    @article = Article.find(params[:id])
+    @comment = @article.comments.create(comment_params)
+  end
+
+  def article
+    @article = Article.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  # get 'static_pages/home'
   resources :articles do
     resources :comments
+  end
+  # get 'static_pages/home'
+  namespace :admin do
+    resources :articles do
+      resources :comments
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
